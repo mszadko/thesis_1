36c7ef87d7c994ffe02df757e4bea834b82cecdc
mszadko <marcin_szadkowski@live.com>
2017-11-06 10:28:58 +0100

    Addd some code to base character

--- Source/thesis_1/BaseCharacter.h
+++ Source/thesis_1/BaseCharacter.h
@@ -26,6 +26,29 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+
+
+	//code added to ACharacter
+private:
+	/*Camera boom*/
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class USpringArmComponent* CameraBoom;
+	/*Camera that follows the player. (We have to think about whether the camera should be player property or there will be main camera that look at whole squad)*/
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* FollowCamera;
+protected:
+	/*Called for applying forward/backward input to character*/
+	void MoveForward(float Value);
+
+	/*Called for applying side to side input to character*/
+	void MoveRight(float Value);
 	
-	
+	/*Called for applying rotation to character (from mouse input)*/
+	void RotateTowardsMouse(float Value);
+
+public:
+	/** Returns CameraBoom subobject **/
+	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+	/** Returns FollowCamera subobject **/
+	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
 };
