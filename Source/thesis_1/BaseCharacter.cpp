d76cebc47849a244ae9163f719a1bb92d0545c27
Maciej Wygoda <maciej.wygoda@gmail.com>
2017-11-27 20:28:42 +0100

    skills arrays now store classes instead of objects
    https://wiki.unrealengine.com/Access_Functions_%26_Variables_From_a_TSubclassOf_Variable_in_C%2B%2B
    
    had to add GetDefaultObject<>() to call functions from classes stored in those arrays

--- Source/thesis_1/BaseCharacter.cpp
+++ Source/thesis_1/BaseCharacter.cpp
@@ -1,13 +1,14 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+
 #include "BaseCharacter.h"
+//#include "Skill.h"
 #include "Camera/CameraComponent.h"
 #include "Components/CapsuleComponent.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "Classes/GameFramework/CharacterMovementComponent.h"
 #include "BasePlayerController.h"
 #include "Engine.h"
-#include "Skill.h"
 
 // Sets default values
 ABaseCharacter::ABaseCharacter()
@@ -42,16 +43,28 @@ ABaseCharacter::ABaseCharacter()
 	RotationSpeed = 0.1f;
 
 	//setting up default skill array
-	Skills.Add(CreateDefaultSubobject<USkill>(TEXT("Skill0")));
-	Skills.Add(CreateDefaultSubobject<USkill>(TEXT("Skill1")));
-	Skills.Add(CreateDefaultSubobject<USkill>(TEXT("Skill2")));
-	Skills.Add(CreateDefaultSubobject<USkill>(TEXT("Skill3")));
+	//Skills.Add(CreateDefaultSubobject<USkill>(TEXT("Skill0")));
+	//Skills.Add(CreateDefaultSubobject<USkill>(TEXT("Skill1")));
+	//Skills.Add(CreateDefaultSubobject<USkill>(TEXT("Skill2")));
+	//Skills.Add(CreateDefaultSubobject<USkill>(TEXT("Skill3")));
+	TSubclassOf<USkill> skillclass0;
+	TSubclassOf<USkill> skillclass1;
+	TSubclassOf<USkill> skillclass2;
+	TSubclassOf<USkill> skillclass3;
+	Skills.Add(skillclass0);
+	Skills.Add(skillclass1);
+	Skills.Add(skillclass2);
+	Skills.Add(skillclass3);
 }
 
 // Called when the game starts or when spawned
 void ABaseCharacter::BeginPlay()
 {
 	Super::BeginPlay();
+	if (ABasePlayerController* BPC = Cast<ABasePlayerController, AController>(GetController()))
+	{
+		Skills = BPC->Skills;
+	}
 }
 
 // Called every frame
